d969fa89e57523af9b44fd72e6fcec6ceb2ba488
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-11 09:22:14 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
+++ src/main/java/jp/co/gyosei/botlog/SecurityConfig.java
@@ -1,10 +1,13 @@
 package jp.co.gyosei.botlog;
 
+import java.sql.SQLException;
+
 import javax.activation.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,6 +15,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -23,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private javax.sql.DataSource dataSource;
 	
 	private static final String USER_QUERY = "SELECT custid, password, effect FROM custinfo WHERE custid = ?";
-	private static final String ROLE_QUERY = "SELECT cid, role FROM custinfo WHERE custid = ?";
+	private static final String ROLE_QUERY = "SELECT custid, role FROM custinfo WHERE custid = ?";
 	
 	@Override
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -61,6 +67,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.invalidateHttpSession(true)
 		.permitAll();
 
+	}
+	
+	@Bean
+	@ConfigurationProperties("spring.datasource")
+	public HikariDataSource dataSource() throws SQLException {
+		if (dbUrl == null || dbUrl.isEmpty()) {
+			return new HikariDataSource();
+		} else {
+			HikariConfig config = new HikariConfig();
+			config.setJdbcUrl(dbUrl);
+			return new HikariDataSource(config);
+		}
 	}
 	
 /*
