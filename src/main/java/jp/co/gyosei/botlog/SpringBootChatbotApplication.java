cea700131a6589d72f2e954f84cd030d77574ea9
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-12 15:47:02 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/SpringBootChatbotApplication.java
+++ src/main/java/jp/co/gyosei/botlog/SpringBootChatbotApplication.java
@@ -1,11 +1,12 @@
 package jp.co.gyosei.botlog;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 //import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootConfiguration
 public class SpringBootChatbotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootChatbotApplication.class, args);
