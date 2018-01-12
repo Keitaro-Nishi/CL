7549fbf0cc86af13f76fef4fe8c0036f3e30e3bc
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-12 10:40:54 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/LoginUserDetails.java
+++ src/main/java/jp/co/gyosei/botlog/LoginUserDetails.java
@@ -1,9 +1,6 @@
 package jp.co.gyosei.botlog;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
