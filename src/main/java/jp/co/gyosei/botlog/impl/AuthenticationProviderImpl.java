5191cbefbae458800f7bad9a415b5f6c4e7fc7f7
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-12 16:12:49 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/impl/AuthenticationProviderImpl.java
+++ src/main/java/jp/co/gyosei/botlog/impl/AuthenticationProviderImpl.java
@@ -27,8 +27,7 @@ public class AuthenticationProviderImpl implements AuthenticationProvider {
 
 		String custid = auth.getName();
 		String password = auth.getCredentials().toString();
-		Collection<? extends GrantedAuthority> role = auth.getAuthorities();
-
+		
 		if ("".equals(custid) || "".equals(password)) {
 			// 例外はSpringSecurityにあったものを適当に使用
 			throw new AuthenticationCredentialsNotFoundException("ログイン情報に不備があります。");
