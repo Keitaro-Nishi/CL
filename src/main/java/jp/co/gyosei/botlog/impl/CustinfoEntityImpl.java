289631869b085164761ae05ab77fe12c81c1f511
Keitaro Nishizawa <keitaro_nishizawa@gyosei-system.co.jp>
2018-01-12 11:08:13 +0900

    s

--- src/main/java/jp/co/gyosei/botlog/impl/CustinfoEntityImpl.java
+++ src/main/java/jp/co/gyosei/botlog/impl/CustinfoEntityImpl.java
@@ -1,62 +1,72 @@
 package jp.co.gyosei.botlog.impl;
 
-import java.io.Serializable;
 import java.util.Collection;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import jp.co.gyosei.botlog.LoginUserDetails;
-
 @Entity
-@Table(name="custinfo")
-public class CustinfoEntityImpl implements Serializable {
+@Table(name = "custinfo")
+public class CustinfoEntityImpl implements UserDetails
+{
+    private static final long serialVersionUID = 1667698003975566301L;
 
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@Column
-	public String custid;
-	@Column
-	public String password;
-	@Column
-	public Collection<? extends GrantedAuthority> role;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long no;
 
-	public CustinfoEntityImpl(String custid, String password, Collection<? extends GrantedAuthority> role) {
- 
-		this.custid = custid;
-		this.password = password;
-		this.role = role;
-	}
+    @Column(nullable = false, unique = true)
+    private String custid;
 
-	public String getCustid() {
-		return this.custid;
-	}
+    @Column(nullable = false)
+    private String password;
 
-	public void setUsername(String custid) {
-		this.custid = custid;
-	}
-
-	public String getPassword() {
-		return this.password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public Collection<? extends GrantedAuthority> getRole() {
-		return role;
-	}
-
-	public void setRole(Collection<? extends GrantedAuthority> role) {
-		this.role = role;
-	}
-	
-    public UserDetails toLoginUserDetails() {
-        return LoginUserDetails.create(this);
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities()
+    {
+        return null;
     }
 
+    @Override
+    public String getPassword()
+    {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername()
+    {
+        return this.custid;
+    }
+
+    @Override
+    public boolean isAccountNonExpired()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired()
+    {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled()
+    {
+        return true;
+    }
 }
